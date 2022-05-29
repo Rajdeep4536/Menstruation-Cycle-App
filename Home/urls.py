7e25006bffba6d3058ed6b41aaedd1384bae2e49
Rajdeep <raorajdeep30@gmail.com>
2022-05-29 16:12:45 +0530

    Initial Commit

---
+++ Home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from Home import views
+app_name='Home'
+
+urlpatterns = [
+  
+  path('', views.index, name='home'),
+  path('contact', views.contact, name ="contact"),
+  path('faq', views.faq, name ="faq"),
+  path('pcod', views.pcod, name ="pcod"),
+  path('selfcare', views.selfcare, name ="selfcare"),  
+]
+
